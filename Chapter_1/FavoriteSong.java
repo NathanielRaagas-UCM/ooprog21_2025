6a4c7da04cfa244393379095b7e6a46b97b78b31
NathanielRaagas-UCM <nathjam12@gmail.com>
2025-09-29 00:29:02 +0800

    Create FavoriteSong.java

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,9 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Open up your heart to me");
+        System.out.println("Open up your heart to me");
+        System.out.println("I knew, u would'nt save me if im fallin in love");
+        System.out.println("Iniibig Kita");
+    
+    }
+}
