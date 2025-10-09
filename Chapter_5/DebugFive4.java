2999453f104df53b4d5b37e817b7b61704bf3c1e
NathanielRaagas-UCM <nathjam12@gmail.com>
2025-10-09 22:27:10 +0800

    Create DebugFive4.java

---
+++ Chapter_5/DebugFive4.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class DebugFive4 {
+    public static void main(String[] args) {
+        int one, two, three, four;
+        int highest;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first integer >> ");
+        one = input.nextInt();
+
+        System.out.print("Enter second integer >> ");
+        two = input.nextInt();
+
+        System.out.print("Enter third integer >> ");
+        three = input.nextInt();
+
+        System.out.print("Enter fourth integer >> ");
+        four = input.nextInt();
+
+        if (one > two && one > three && one > four)
+            highest = one;
+        else if (two > one && two > three && two > four)
+            highest = two;
+        else if (three > one && three > two && three > four)
+            highest = three;
+        else
+            highest = four;
+
+        System.out.println("The highest number is " + highest);
+    }
+}
