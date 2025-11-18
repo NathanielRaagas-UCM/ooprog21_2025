54a8dd22d3abd86fd5797131fb1887a93bf94fc8
NathanielRaagas-UCM <nathjam12@gmail.com>
2025-11-18 21:31:02 +0800

    Update Payroll.java

--- Chapter_5/Payroll.java
+++ Chapter_5/Payroll.java
@@ -10,7 +10,7 @@ public class Payroll {
         System.out.print("What is your regular pay rate? ");
         double payRate = input.nextDouble();
 
-        // Validate pay rate
+        
         if (payRate > 60.00) {
             System.out.println("Pay rate exceeds the maximum of 60.00. Setting to 60.00.");
             payRate = 60.00;
@@ -19,7 +19,7 @@ public class Payroll {
         double regularPay;
         double overtimePay = 0.0;
 
-        // Calculate regular and overtime pay
+        
         if (hoursWorked <= 40) {
             regularPay = hoursWorked * payRate;
         } else {
@@ -27,7 +27,7 @@ public class Payroll {
             overtimePay = (hoursWorked - 40) * payRate * 1.5;
         }
 
-        // Display results
+        
         System.out.println("Regular pay is " + regularPay);
         System.out.println("Overtime pay is " + overtimePay);
 
